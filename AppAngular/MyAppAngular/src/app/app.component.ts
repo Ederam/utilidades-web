f833994d7bf2e21f22e2752f09c43cde5fbaf83e
Ederam <53803895+Ederam@users.noreply.github.com>
2025-03-11 15:29:43 -0500

    Adicion de codigo html, css en comp.ts, lectura variablas, ejecucion de codigo javascript, agregar estilos globales

--- AppAngular/MyAppAngular/src/app/app.component.ts
+++ AppAngular/MyAppAngular/src/app/app.component.ts
@@ -4,8 +4,15 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  // templateUrl: './app.component.html',
+  template: `
+  <h1>Hola a todos,enviando codigo html y estilo css desde el componente principal de la aplicacion "app.component.ts"</h1>
+  <h1>La aplicación se llama {{title}}</h1>
+  <h1>La aplicación se llama {{title.toUpperCase()}} usando funcion de javascript</h1>
+  `,
+  // styleUrl: './app.component.css'
+  styles: `h1{
+    color: azul}`
 })
 export class AppComponent {
   title = 'MyAppAngular';
