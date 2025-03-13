1f0297d8f067a148d053d72c971012f446ab7aba
Ederam <53803895+Ederam@users.noreply.github.com>
2025-03-13 12:39:15 -0500

    directiva @if, @else
    Se añade directiva y un section para darle estilos

--- AppAngular/MyAppAngular/src/app/app.component.ts
+++ AppAngular/MyAppAngular/src/app/app.component.ts
@@ -5,18 +5,19 @@ import { UserComponent } from './user/user.component';
 @Component({
   selector: 'app-root',
   imports: [UserComponent],
-  // templateUrl: './app.component.html',
-  template: `
-  <h1>Hola a todos,enviando codigo html y estilo css desde el componente principal de la aplicacion "app.component.ts"</h1>
-  <h1>La aplicación se llama {{title}}</h1>
-  <h1>La aplicación se llama {{title.toUpperCase()}} usando funcion de javascript</h1>
-  <app-user></app-user>
-  <!-- se puede usar varias veces y tambien de la otra manera <app-user/>-->
-  <app-user/><app-user/>
-  `,
+  // imports: [UserComponent],
+  templateUrl: './app.component.html',
+  // template: `
+  // <h1>Hola a todos,enviando codigo html y estilo css desde el componente principal de la aplicacion "app.component.ts"</h1>
+  // <h1>La aplicación se llama {{title}}</h1>
+  // <h1>La aplicación se llama {{title.toUpperCase()}} usando funcion de javascript</h1>
+  // <app-user></app-user>
+  // <!-- se puede usar varias veces y tambien de la otra manera <app-user/>-->
+  // <app-user/><app-user/>
+  // `,
   // styleUrl: './app.component.css'
-  styles: `h1{
-    color: azul}`
+  // styles: `h1{
+  //   color: azul}`
 })
 export class AppComponent implements OnInit{
   
