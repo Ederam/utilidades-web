d0d21dc3e6ffd1ce2e54a5e75008d82149560428
Ederam <53803895+Ederam@users.noreply.github.com>
2025-03-13 18:21:39 -0500

    Carga diferida de datos, componente de comments, @defer, @placeholder, @loading

--- AppAngular/MyAppAngular/src/app/app.component.ts
+++ AppAngular/MyAppAngular/src/app/app.component.ts
@@ -1,23 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserComponent } from './user/user.component';
+import { CommentsComponent } from "./comments/comments.component";
 
 @Component({
   selector: 'app-root',
-  imports: [UserComponent],
-  // imports: [UserComponent],
-  templateUrl: './app.component.html',
-  // template: `
-  // <h1>Hola a todos,enviando codigo html y estilo css desde el componente principal de la aplicacion "app.component.ts"</h1>
-  // <h1>La aplicación se llama {{title}}</h1>
-  // <h1>La aplicación se llama {{title.toUpperCase()}} usando funcion de javascript</h1>
-  // <app-user></app-user>
-  // <!-- se puede usar varias veces y tambien de la otra manera <app-user/>-->
-  // <app-user/><app-user/>
-  // `,
-  // styleUrl: './app.component.css'
-  // styles: `h1{
-  //   color: azul}`
+  imports: [CommentsComponent],  
+  templateUrl: './app.component.html',  
+  styleUrl: './app.component.css'
+  
 })
 export class AppComponent implements OnInit{
   
