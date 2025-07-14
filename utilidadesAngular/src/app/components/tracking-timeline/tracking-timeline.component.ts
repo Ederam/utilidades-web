611ae578d492381b99a2228261b86da2955efa0b
edercramirezc <ingeniero.master41@interrapidisimo.com>
2025-07-14 14:35:36 -0500

    Se agrega nueva aplicacion para provbar codigo

---
+++ utilidadesAngular/src/app/components/tracking-timeline/tracking-timeline.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tracking-timeline',
+  imports: [],
+  templateUrl: './tracking-timeline.component.html',
+  styleUrl: './tracking-timeline.component.css'
+})
+export class TrackingTimelineComponent {
+
+}
